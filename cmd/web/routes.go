9d75980f48caa38d269b961bc9e1d591ac16b38d
Daniel Ola-Olorun <danielolaolorun@gmail.com>
2024-02-26 14:01:56 -0800

    first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/assets"
+
+	"github.com/gorilla/mux"
+)
+
+func (app *application) routes() http.Handler {
+	mux := mux.NewRouter()
+	mux.NotFoundHandler = http.HandlerFunc(app.notFound)
+
+	mux.Use(app.logAccess)
+	mux.Use(app.recoverPanic)
+	mux.Use(app.securityHeaders)
+
+	fileServer := http.FileServer(http.FS(assets.EmbeddedFiles))
+	mux.PathPrefix("/static/").Handler(fileServer)
+
+	mux.HandleFunc("/", app.home).Methods("GET")
+
+	protectedRoutes := mux.NewRoute().Subrouter()
+	protectedRoutes.Use(app.requireBasicAuthentication)
+	protectedRoutes.HandleFunc("/basic-auth-protected", app.protected).Methods("GET")
+
+	return mux
+}
