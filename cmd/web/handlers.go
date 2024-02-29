bcbc6c678bc0bc6ac7515b3c97df16306492eeb0
Daniel Ola-Olorun <danielolaolorun@gmail.com>
2024-02-29 12:19:27 -0800

    added handlers for privacypolicy, tos, disclaimer, generate, login, signup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,6 +15,47 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+func (app *application) privacypolicy(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	err := response.Page(w, http.StatusOK, data, "pages/privacypolicy.tmpl")
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) disclaimer(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	err := response.Page(w, http.StatusOK, data, "pages/disclaimer.tmpl")
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+
+func (app *application) tos(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	err := response.Page(w, http.StatusOK, data, "pages/termsofservice.tmpl")
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) login(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
+func (app *application) signup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
+
+
 func (app *application) protected(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is a protected handler"))
 }
