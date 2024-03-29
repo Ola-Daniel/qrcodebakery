b02ece3192980f5954b32452f7b76a7058835331
Daniel Ola-Olorun <danielolaolorun@gmail.com>
2024-03-29 23:18:33 +0100

    migration script updates

--- assets/migrations/qr_codes.sql
+++ assets/migrations/qr_codes.sql
@@ -2,5 +2,7 @@ CREATE TABLE qr_codes (
     qrcode_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     data TEXT NOT NULL,
+    image_path TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
